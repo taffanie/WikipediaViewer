5853e3b975fe8dec53be501f974e9594d0a03e01
taffanie <46627573+taffanie@users.noreply.github.com>
2019-12-23 19:25:19 +0000

    Upload

---
+++ script.js
@@ -0,0 +1,62 @@
+$(function(){
+  
+  ///w/api.php?action=query&format=json&prop=revisions&titles=Main%20Page&rvprop=content
+  
+  //"https://en.wikipedia.org/w/api.php?action=opensearch&search=billy&callback=?
+  
+  ///wiki/api/.php?action=opensearch&search=billy&callback=?
+  
+  //https://en.wikipedia.org/w/api.php?action=opensearch&search=billy&callback=?
+  
+  $('#search').click(function(){
+    
+    let searchTerm1 = $('#searchTerm').val(); 
+    
+    $.ajax({
+      
+      type: 'GET',
+      url: 'https://en.wikipedia.org/w/api.php?action=opensearch&search=' + searchTerm1 + '&callback=?',
+      async: false,
+      dataType: 'json',
+      success: function(data){
+        //console.log(data[3][1]);
+        
+        $('#results').html('');
+        
+        for(let i=0; i<data[1].length; i++){
+          $('#results').append('<a href=' + data[3][i] + 'target="blank">' + data[1][i] + '</a>' + '<br>')
+        
+          
+          
+        }
+        
+      
+    },
+      error: function(err){
+        alert("Error")
+      }
+     
+      
+
+      
+      
+    })
+      
+      
+
+    
+    
+    
+    
+  })
+  
+  
+  //add type enter on keyboard to search 
+  $("#searchTerm").keyup(function(event) {
+    if (event.keyCode == 13) {
+        $("#search").click();
+    }
+});
+     
+  
+});
